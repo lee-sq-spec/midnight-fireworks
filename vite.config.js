b83451bf844cad84a7738cf19e33b9ef9add6aa2
lee-sq-spec <m-11750204@moe-dl.edu.my>
2025-12-27 10:56:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',
   base: './',
-  server: {
-    port: 5173,
-    open: true
-  }
+  build: { outDir: 'dist', assetsDir: 'assets' }
 });
